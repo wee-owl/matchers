8d487a73badc8eafd9040034f2525889e28bbc18
wee-owl <kem-cv@yandex.ru>
2023-04-26 12:43:27 +0300

    feat: update build and check test coverage

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,13 +1,17 @@
-import lifeIndication from '../app';
+import sortHeroes from '../app';
 
-test.each([
-  { name: 'мечник', health: 10, expected: 'critical' },
-  { name: 'маг', health: 100, expected: 'healthy' },
-  { name: 'лучник', health: 45, expected: 'wounded' },
-])(
-  'determine lifelevel %s of health %i',
-  ({ name, health, expected }) => {
-    const result = lifeIndication({ name, health, expected });
-    expect(result).toBe(expected);
-  },
-);
+const array = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+
+const result = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+];
+
+test('sort ascending', () => {
+  expect(sortHeroes(array)).toEqual(result);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,3 @@
-export default function lifeIndication(hero) {
-  let lifeLevel = '';
-  if (hero.health > 50) {
-    lifeLevel = 'healthy';
-  } else if (hero.health < 15) {
-    lifeLevel = 'critical';
-  } else {
-    lifeLevel = 'wounded';
-  }
-  return lifeLevel;
+export default function sortHeroes(array) {
+  return array.sort((a, b) => b.health - a.health);
 }
